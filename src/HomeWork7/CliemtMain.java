50708ea7831d757af6e0a26bcbc15c46778f9263
geekbrains <olgaget@inbox.ru>
2022-02-04 09:19:13 +0600

    Initial commit

---
+++ src/HomeWork7/CliemtMain.java
@@ -0,0 +1,7 @@
+package HomeWork7;
+
+public class CliemtMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+    }
+}
---
+++ src/HomeWork7/ServerMain.java
@@ -0,0 +1,7 @@
+package HomeWork7;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
